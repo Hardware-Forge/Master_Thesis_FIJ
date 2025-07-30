aacc3702bf4734b9e165d3124f51d110d7b0693d
Carbo2504 <carbonettiandrea@gmail.com>
2025-07-30 10:59:23 +0200

    feat: program started by module + injection at beginning of program

--- fij_command/fij.c
+++ fij_command/fij.c
@@ -8,10 +8,13 @@
 #define IOCTL_START_FAULT _IOW('f', 1, struct fij_params)
 #define IOCTL_STOP_FAULT  _IO('f', 2)
 #define IOCTL_STATUS_FAULT _IOR('f', 3, int)
+#define IOCTL_EXEC_AND_FAULT _IOW('f', 4, struct fij_params)
 
 struct fij_params {
     char process_name[256];
-    int cycles;
+    char process_path[256];     // program path
+    char process_args[256];     // argument string
+    int cycles;                 // fault cycles
 };
 
 int main(int argc, char *argv[]) {
@@ -59,11 +62,50 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         printf("Status: %s\n", status ? "Running" : "Idle");
-    } else {
+    } else if (argc >= 2 && strcmp(argv[1], "exec") == 0) {
+        struct fij_params params = {0};
+        params.cycles = 0;  // default to infinite
+
+        for (int i = 2; i < argc; ++i) {
+            if (strncmp(argv[i], "path=", 5) == 0) {
+                strncpy(params.process_path, argv[i] + 5, sizeof(params.process_path) - 1);
+		// Auto-fill process_name from process_path (e.g., /usr/bin/echo -> echo)
+		char *last_slash = strrchr(params.process_path, '/');
+		if (last_slash)
+    		  strncpy(params.process_name, last_slash + 1, sizeof(params.process_name) - 1);
+		else
+    		  strncpy(params.process_name, params.process_path, sizeof(params.process_name) - 1);
+            } else if (strncmp(argv[i], "args=", 5) == 0) {
+                strncpy(params.process_args, argv[i] + 5, sizeof(params.process_args) - 1);
+            } else if (strncmp(argv[i], "cycles=", 7) == 0) {
+                params.cycles = atoi(argv[i] + 7);
+            } else {
+                fprintf(stderr, "Invalid argument: %s\n", argv[i]);
+                return 1;
+            }
+        }
+
+        if (params.process_path[0] == '\0') {
+            fprintf(stderr, "Missing path= argument\n");
+            return 1;
+        }
+
+        if (ioctl(fd, IOCTL_EXEC_AND_FAULT, &params) < 0) {
+            perror("ioctl exec");
+            return 1;
+        }
+
+        printf("Executed '%s' with args '%s' and injected faults (%s cycles)\n",
+               params.process_path,
+               params.process_args[0] ? params.process_args : "(none)",
+               params.cycles == 0 ? "infinite" : argv[3]);
+    }
+    else {
         fprintf(stderr, "Usage:\n");
         fprintf(stderr, "%s start process=NAME [cycles=N]\n", argv[0]);
         fprintf(stderr, "  %s stop\n", argv[0]);
         fprintf(stderr, "  %s status\n", argv[0]);
+        fprintf(stderr, "  %s exec path=PATH [args=\"ARG1 ARG2\"] [cycles=N]\n", argv[0]);	
         return 1;
     }
 
--- fij_mod.c
+++ fij_mod.c
@@ -24,14 +24,18 @@ MODULE_VERSION("0.2");
 
 #define DEVICE_NAME "fij"
 #define CLASS_NAME  "fij_class"
+#define MAX_ARGC 6
 
 #define IOCTL_START_FAULT  _IOW('f', 1, struct fij_params)
 #define IOCTL_STOP_FAULT   _IO('f', 2)
 #define IOCTL_GET_STATUS   _IOR('f', 3, int)
+#define IOCTL_EXEC_AND_FAULT _IOW('f', 4, struct fij_params)
 
 struct fij_params {
     char process_name[256];
-    int cycles;
+    char process_path[256];      // e.g., "/usr/bin/myapp"
+    char process_args[256];      // e.g., "arg1 arg2"
+    int cycles;                  // Number of bitflips to inject (0 = infinite)
 };
 
 /************** Globals **************/
@@ -163,6 +167,13 @@ static int bitflip_thread_fn(void *data) {
     running = 0;
     return 0;
 }
+
+    /************** Inject SIGSTOP Before Exec **************/
+    static int helper_child_init(struct subprocess_info *info, struct cred *new) {
+        // This executes in the child process context before exec
+        send_sig(SIGSTOP, current, 0);  // Stop self
+        return 0;
+    }
 /************** IOCTL Handler **************/
 static long fij_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     struct fij_params params;
@@ -209,7 +220,105 @@ static long fij_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
         if (put_user(running, (int __user *)arg))
             return -EFAULT;
         break;
+case IOCTL_EXEC_AND_FAULT: {
+    struct fij_params params;
+    char *argv[MAX_ARGC + 2]; // binary + args + NULL
+    char *envp[] = { "HOME=/", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
+    char *path_copy = NULL, *args_copy = NULL, *token = NULL;
+    int argc = 0, ret = 0;
+    struct subprocess_info *sub_info;
 
+    if (copy_from_user(&params, (void __user *)arg, sizeof(params)))
+        return -EFAULT;
+
+    if (running)
+        return -EBUSY;
+
+    // Set cycles
+    remaining_cycles = (params.cycles == 0) ? -1 : params.cycles;
+    running = 1;
+
+    path_copy = kstrdup(params.process_path, GFP_KERNEL);
+    if (!path_copy)
+        return -ENOMEM;
+
+    // Construct argv[]
+    argv[argc++] = path_copy;
+
+    if (params.process_args[0]) {
+        args_copy = kstrdup(params.process_args, GFP_KERNEL);
+        if (!args_copy) {
+            kfree(path_copy);
+            return -ENOMEM;
+        }
+
+        while ((token = strsep(&args_copy, " ")) != NULL && argc < MAX_ARGC + 1) {
+            if (*token == '\0') continue;
+            argv[argc++] = token;
+        }
+    }
+
+    argv[argc] = NULL;
+
+    sub_info = call_usermodehelper_setup(path_copy, argv, envp, GFP_KERNEL,
+                                         helper_child_init, NULL, NULL);
+    if (!sub_info) {
+        kfree(path_copy);
+        return -ENOMEM;
+    }
+
+    ret = call_usermodehelper_exec(sub_info, UMH_WAIT_EXEC);
+    if (ret) {
+        printk(KERN_ERR "fij: usermodehelper_exec failed (%d)\n", ret);
+        running = 0;
+        kfree(path_copy);
+        return ret;
+    }
+
+    // At this point, the child is paused (SIGSTOP), and exec hasn't run yet
+
+    // Find the PID by process name
+    target_pid = find_pid_by_name(params.process_name);
+    if (target_pid < 0) {
+        printk(KERN_ERR "fij: launched process '%s' not found\n", params.process_name);
+        running = 0;
+        kfree(path_copy);
+        return -ESRCH;
+    }
+
+    printk(KERN_INFO "fij: injecting fault into '%s' (PID %d)\n", params.process_name, target_pid);
+
+    // Inject fault before it starts executing
+    ret = perform_bitflip();
+
+    // Resume execution after injection
+    {
+        struct pid *pid_struct = find_get_pid(target_pid);
+        if (pid_struct) {
+            send_sig(SIGCONT, pid_task(pid_struct, PIDTYPE_PID), 0);
+            put_pid(pid_struct);
+            printk(KERN_INFO "fij: sent SIGCONT to PID %d\n", target_pid);
+        } else {
+            printk(KERN_ERR "fij: failed to get PID struct for PID %d\n", target_pid);
+        }
+    }
+
+    // Start bitflip thread (if more cycles are needed)
+    if (remaining_cycles != 1) {
+        bitflip_thread = kthread_run(bitflip_thread_fn, NULL, "bitflip_thread");
+        if (IS_ERR(bitflip_thread)) {
+            printk(KERN_ERR "fij: failed to start bitflip thread\n");
+            running = 0;
+            kfree(path_copy);
+            return PTR_ERR(bitflip_thread);
+        }
+    } else {
+        running = 0; // Only one injection was requested and already done
+    }
+
+    kfree(path_copy); // args_copy was consumed in-place
+    return ret;
+}
     default:
         return -EINVAL;
     }
